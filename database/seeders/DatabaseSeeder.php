33587307b8e4196db2063fed10e2a462ac045500
igorforspeak@gmail.com <igorforspeak@gmail.com>
2024-04-16 13:22:22 +0300

    seeder для создание пользователей

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::firstOrCreate(
+            ['email' => 'test@ex.com'],
+            [
+                'name' => 'Test User',
+                'password' => 1234,
+                'is_admin' => false,
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@ex.com'],
+            [
+                'name' => 'Admin User',
+                'password' => 1234,
+                'is_admin' => true,
+            ]
+        );
     }
 }
